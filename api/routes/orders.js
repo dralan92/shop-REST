f8b8eb43a0c54fec631327f9e9a314d0e3cbea9a
unknown <codename100.alan@gmail.com>
2018-09-24 01:33:53 -0400

    Order deleted

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -49,4 +49,20 @@ router.get('/:orderId', (req,res,next)=>{
         id : id
     });
 });
+
+router.delete('/:orderId', (req, res, next )=>{
+    Order.remove({_id : req.params.orderId})
+    .exec()
+    .then(result =>{
+        res.status(200).json({
+            message : 'order deleted',
+            
+        })
+    })
+    .catch(err=>{
+        res.status(500).json({
+            error : err
+        })
+    });
+});
 module.exports = router;
